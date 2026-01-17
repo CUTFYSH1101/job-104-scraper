1e5beb5af4faaf01d75ee5035f183612f0a6dbea
CUTFYSH1101 <cut.fysh.1101@gmail.com>
2026-01-17 13:26:13 +0800

    performance：預計算

--- src/components/results/KeywordCoverageResult.vue
+++ src/components/results/KeywordCoverageResult.vue
@@ -117,11 +117,12 @@ export default {
 
       this.loading = true
       this.loadingStep = '1/4'
-      await this.calcuTotalRate()
+      await this.preCalcuMatches()
       this.loadingStep = '2/4'
-      await this.calcuRateForeachJob()
+      this.calcuTotalRate()
       this.loadingStep = '3/4'
-      await this.calcuWeightForeachJob()
+      this.calcuRateForeachJob()
+      this.calcuWeightForeachJob()
       this.loadingStep = '4/4'
       this.sortJobs()
       this.loading = false
@@ -146,28 +147,31 @@ export default {
         }
       }
     },
-    // 這個工作的技能要求中，有多少比例符合你搜尋的關鍵字，越大表示這個工作越符合你的搜尋條件
-    async calcuRateForeachJob() {
+    async preCalcuMatches() {
       await batcher.forEach(this.processedJobs, job => {
+        job.mustMatches = this.mustKeywords.filter(key => isJobTagsMatchKeyword(job, key))
+        job.notMatches = this.notKeywords.filter(key => isJobTagsMatchKeyword(job, key))
+      })
+    },
+    // 這個工作的技能要求中，有多少比例符合你搜尋的關鍵字，越大表示這個工作越符合你的搜尋條件
+    calcuRateForeachJob() {
+      this.processedJobs.forEach(job => {
         let total = this.processedKeywords.length
-        let must = this.mustKeywords.filter(key => isJobTagsMatchKeyword(job, key))
-        let not = this.notKeywords.filter(key => !isJobTagsMatchKeyword(job, key))  // 全都不包含算符合一次
-        let count = must.length + not.length
+        let notMisses = this.notKeywords.length - job.notMatches.length  // notMisses: 每少一個負面就加分
+        let count = job.mustMatches.length + notMisses
         job.skillRate = count / total
       })
       console.log('rate')
     },
     // 對這份工作來說，關鍵字佔多重，越重表示這份工作越符合你
-    async calcuWeightForeachJob() {
-      await batcher.forEach(this.processedJobs, job => {
+    calcuWeightForeachJob() {
+      this.processedJobs.forEach(job => {
         let total = getTags(job).length
-        let must = this.mustKeywords.filter(key => isJobTagsMatchKeyword(job, key))
-        let not = this.notKeywords.filter(key => isJobTagsMatchKeyword(job, key))  // 每有一個就扣分
-        let score = must.length - not.length
+        let score = job.mustMatches.length - job.notMatches.length  // notMatches: 每有一個負面就扣分
         if (score < 0) score = 0
         if (score > total) score = total
         job.skillWeight = score / total
-        job.skillWeightHint = `${prefixEach(must, '+')}${prefixEach(not, '-')}共${score}項符合（${must.length}-${not.length}），${score}/${total} = ${job.skillWeight}`
+        job.skillWeightHint = `${prefixEach(job.mustMatches, '+')}${prefixEach(job.notMatches, '-')}共${score}項符合（${job.mustMatches.length}-${job.notMatches.length}），${score}/${total} = ${job.skillWeight}`
       })
       console.log('weight')
     },
@@ -182,25 +186,25 @@ export default {
       })
       console.log('排序工作')
     },
-    async calcuTotalRate() {
+    calcuTotalRate() {
       // python佔6成，表示個別關鍵字佔整體市場多少工作
       let total = this.processedJobs.length
       this.skillRateDict = {}
       this.mustKeywords.forEach(key => {
-        let count = utils.count(this.processedJobs, job => isJobTagsMatchKeyword(job, key))
+        let count = utils.count(this.processedJobs, job => job.mustMatches.includes(key))
         this.skillRateDict[key] = count / total
       })
       this.notKeywords.forEach(key => {
-        let count = utils.count(this.processedJobs, job => !isJobTagsMatchKeyword(job, key))
+        let count = utils.count(this.processedJobs, job => !job.notMatches.includes(key))
         this.skillRateDict['-' + key] = count / total
       })
 
       // 當一個工作每個關鍵字都符合，越多工作符合，表示該關鍵字可以代表很大的市場
       let count = 0
-      await batcher.forEach(this.processedJobs, job => {
-        if (!this.mustKeywords.every(key => isJobTagsMatchKeyword(job, key)))
+      this.processedJobs.forEach(job => {
+        if (this.mustKeywords.length !== job.mustMatches.length)  // every
           return
-        if (this.notKeywords.some(key => isJobTagsMatchKeyword(job, key)))
+        if (job.notMatches.length > 0)  // some
           return
         count++
       })
@@ -214,15 +218,15 @@ export default {
     },
   },
   watch: {
-    keyword() {
-      this.updateResult()
+    async keyword() {
+      await this.updateResult()
     },
   },
   updated() {
     SetJobsAndPoses.updated(this.$refs.jobsView)
   },
-  activated() {
-    this.updateResult()
+  async activated() {
+    await this.updateResult()
     SetJobsAndPoses.activated(this.$refs.jobsView)
     if (!this.keyword) this.loading = false
   },
